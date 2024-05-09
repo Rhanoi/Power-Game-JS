4e6bf54c7911e016eeafa3c6f69557f9f794d3f2
Renan Mota <renanmotadasilva@outlook.com>
2024-05-09 18:55:46 -0300

    Implementando Funções e finalizando o projeto

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //importando funçoes de outro arquivo JavaScript
-import {acaoEsfera} from './src/funcoes.js'
+import {acaoEsfera, classPower, xp} from './src/funcoes.js'
+
 
 //Importando promptSync que permite uma interação com o usuário no terminal
 import promptSync from 'prompt-sync';
@@ -9,20 +10,65 @@ const prompt = promptSync();
 const guilda = "Power Up"
 let esfera
 let heroi = "Viajante: "
-let nextQuestion = "\n---------------------------------------------------------------------------------------\n\n"
+export let nextQuestion = "\n---------------------------------------------------------------------------------------\n\n"
 
-console.log("Olá e bem vindo a guilda "+ guilda+"!\nQual é o seu nome?")
+console.log("Olá e bem vindo a guilda "+ guilda+"! Me chamo Renan e sou o lider por aqui!\nQual é o seu nome?")
 heroi = prompt(heroi+"")
 
 console.log(nextQuestion+
-            "Então você quer fazer parte da guilda mais forte da região né?\n"+
+            "Renan: Então você quer fazer parte da guilda mais forte da região né?\n"+
     "Mas para isso precisamos medir o seu real nível de poder para sabermos quais\nmissões são mais adequadas para tí!"+
     "Nosso medidor de poder vai te ajudar com isso!"
 )
 
 
-console.log("Você! vê uma esfera de cristal que muda cor\n")
-console.log("Fazer o teste?\nDigite:\n1 para sim\n2 para não\n")
+console.log("---Você! vê uma esfera de cristal que muda cor---\n")
+console.log("\nRenan: Ele vai medir seu xp atual: \n\n"+
+"Se XP for menor do que 1.000 = Ferro\n"+
+"Se XP for entre 1.001 e 2.000 = Bronze\n"+
+"Se XP for entre 2.001 e 5.000 = Prata\n"+
+"Se XP for entre 5.001 e 7.000 = Ouro\n"+
+"Se XP for entre 7.001 e 8.000 = Platina\n"+
+"Se XP for entre 8.001 e 9.000 = Ascendente\n"+
+"Se XP for entre 9.001 e 10.000= Imortal\n"+
+"Se XP for maior ou igual a 10.001 = Radiante\n\n")
+console.log("Deseja fazer o teste agora?\nDigite:\n1 para sim\n2 para não\n")
+
+
 esfera = parseInt(prompt(heroi+": "))
 
 acaoEsfera(esfera, heroi)
+
+console.log(nextQuestion+"Renan: Bom, vejamos... seu Xp é : "+ xp+ "\n"+
+            "então sua classe será... "+ classPower+"!")
+console.log("Está decidido! Oficialmente o "+ heroi+ ", pertence a classe "+classPower+"!")
+console.log("\n\nTodos da Guilda: Bem vindo "+heroi+"!")
+console.log(nextQuestion+"Renan: Grandes aventuras o aguardam!\nVenha sempre a base da guilda para verificar as missoes disponíveis para a sua classe de poder!")
+console.log("Até breve!")
+
+
+
+/*
+# 1️⃣ Desafio Classificador de nível de Herói
+
+**O Que deve ser utilizado**
+
+
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 5.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000= Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói de nome **{nome}** está no nível de **{nivel}**"
+*/
--- src/funcoes.js
+++ src/funcoes.js
@@ -1,21 +1,75 @@
+//possibilitando entrada de dados pelo terminal
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
+//variáveis
+import {nextQuestion} from '../index.js'
+export let classPower
+export let xp
 
-export function acaoEsfera(resposta, heroi){
-    do{
-        if (isNaN(resposta)) {
-            resposta = parseInt(prompt("Entrada inválida. Digite um número: "));
+
+//Calculando o poder via Math.ramdom(), ao multiplicar por 100 temos os números de 0 a 99
+export function calculoPower(poder){
+        poder = Math.floor(Math.random() * 10050);
+        xp = poder
+        if (poder<1000){
+            poder = "Ferro"
+        } else if (poder<=2000){
+            poder = "Bronze"
+        }else if (poder<=5000){
+            poder = "Prata"
+        }else if (poder<=7000){
+            poder = "Ouro"
+        }else if (poder<=8000){
+            poder = "Platina"
+        }else if (poder<=9000){
+            poder = "Ascendente"
+        }else if (poder<=1000){
+            poder = "Imortal"
+        }else{
+            poder = "Radiante"
         }
-    }while(isNaN(resposta))
+        classPower = poder
+}
 
-    while(resposta < 1 || resposta > 2){
-        console.log("Digite um comando válido")
-        resposta = parseInt(prompt(heroi+": "))
-    }
+export function acaoEsfera(resposta, poder){
+    do{
+        if (isNaN(resposta) || resposta < 1 || resposta > 2) {
+            resposta = parseInt(prompt("Entrada inválida. Digite 1 para sim ou 2 para não: "))
+        } else if(resposta == 2){
+            resposta = parseInt(prompt("Não tenha medo de testar seu poder! Digite 1 para sim ou 2 para não: "))
+        }
+    }while(isNaN(resposta) || resposta != 1)
+   
     if (resposta == 1){
-    console.log("você digitou 1")
-    }else if(resposta == 2){
-    console.log("você digitou 2")
+        calculoPower(classPower)
     }
 }
+/*
+# 1️⃣ Desafio Classificador de nível de Herói
+
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 5.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000= Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói de nome **{nome}** está no nível de **{nivel}**"
+*/
